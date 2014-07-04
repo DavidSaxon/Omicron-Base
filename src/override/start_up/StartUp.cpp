583b29aa60faa63a8283da92f9a63eb12e0fd9d7
David Saxon <davidesaxon@gmail.com>
2014-07-05 01:13:16 +1200

    cmake and build for linux

---
+++ src/override/start_up/StartUp.cpp
@@ -0,0 +1,15 @@
+//TODO: include settings
+#include <iostream>
+
+namespace start_up {
+
+/**This function is called at initialisation of Omicron, it's used to set the
+initial settings and provided the first scene to run
+@return the first scene to run*/
+//TODO: return scene
+void init() {
+
+	std::cout << "init settings" << std::endl;
+}
+
+} //namespace start_up
---
+++ src/override/start_up/StartUp.hpp
@@ -0,0 +1,14 @@
+#ifndef OMICRON_OVERRIDE_STARTUP_STARTUP_H_
+#	define OMICRON_OVERRIDE_STARTUP_STARTUP_H_
+
+/**************************************************\
+| functions used to initialise settings at launch. |
+\**************************************************/
+namespace start_up {
+
+
+//TODO:
+
+} //namespace startup
+
+#endif
