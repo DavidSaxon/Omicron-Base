1d51b454bba812fed2ef4b6b2635a72724526955
David Saxon <davidesaxon@gmail.com>
2014-10-19 22:06:23 +1300

    final update before transferring changes to master

---
+++ src/data/layers/LevelLayers.hpp
@@ -0,0 +1,16 @@
+#ifndef OMICRON_DATA_LAYERS_LEVELLAYERS_HPP
+#   define OMICRON_DATA_LAYERS_LEVELLAYERS_HPP
+
+namespace layer {
+
+namespace level {
+
+enum Layer {
+
+}
+
+} // namespace level
+
+} // namespace layer
+
+#endif
