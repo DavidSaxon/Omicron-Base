8f3d6a4f6eec57754a9f805479d06d8dd12149a5
David Saxon <davidesaxon@gmail.com>
2014-10-23 00:27:08 +1300

    restarted lighting work

--- res/gfx/shader/omicron/default_fragment.glsl
+++ res/gfx/shader/omicron/default_fragment.glsl
@@ -28,19 +28,22 @@ uniform float u_PointDis[8];
 // the point light colours
 uniform vec3 u_PointColour[8];
 
-//the vertex coords
-varying vec3 v_vertex;
 //the texture coords
 varying vec2 v_texCoord;
+// TESTING
+varying vec4 v_colour;
+
+//the vertex coords
+varying vec3 v_vertex;
 //the normals
 varying vec3 v_normal;
 
-// the vertex position in camera space
-varying vec3 v_vertexCameraSpace;
-// the direction of the eye
-varying vec3 v_eyeDirection;
-// the normal in camera space
-varying vec3 v_normalCameraSpace;
+// // the vertex position in camera space
+// varying vec3 v_vertexCameraSpace;
+// // the direction of the eye
+// varying vec3 v_eyeDirection;
+// // the normal in camera space
+// varying vec3 v_normalCameraSpace;
 
 //------------------------------------------------------------------------------
 //                                 MAIN FUNCTION
@@ -51,7 +54,8 @@ void main() {
     //---------------------------------MATERIAL---------------------------------
 
     // the colour of the material
-    vec4 material = u_colour;
+    // vec4 material = u_colour;
+    vec4 material = v_colour;
 
     //apply texturing
     vec4 textureColour = texture2D(u_texture, v_texCoord);
@@ -62,66 +66,68 @@ void main() {
 
     //---------------------------------LIGHTING---------------------------------
 
-    if ( u_shadeless != 0 )
-    {
-        gl_FragColor = material;
-    }
-    else
-    {
-        // normalize normal
-        vec3 N = normalize( v_normal );
+    gl_FragColor = material;
 
-        // apply ambient light
-        vec3 light = u_ambientLight;
+    // if ( u_shadeless != 0 )
+    // {
+    //     gl_FragColor = material;
+    // }
+    // else
+    // {
+    //     // normalize normal
+    //     vec3 N = normalize( v_normal );
 
-        // apply point lights
-        for ( int i = 0; i < u_PointCount; ++i )
-        {
-            vec3 L = normalize( u_PointPos[i] - v_vertex );
-            vec3 E = normalize( -v_vertex );
-            vec3 R = normalize( -reflect( L, N ) );
+    //     // apply ambient light
+    //     vec3 light = u_ambientLight;
 
-            // calculate diffuse
-            vec3 diffuse = u_PointColour[i] * max( dot( N, L ), 0.0 );
-            diffuse = clamp( diffuse, 0.0, 1.0 );
+    //     // apply point lights
+    //     for ( int i = 0; i < u_PointCount; ++i )
+    //     {
+    //         vec3 L = normalize( u_PointPos[i] - v_vertex );
+    //         vec3 E = normalize( -v_vertex );
+    //         vec3 R = normalize( -reflect( L, N ) );
 
-            // calculate specular
-            vec3 specular = vec3( 1.0 ) * pow( max( dot( R, E ), 0.0 ), 50.0 );
-            specular = clamp( specular, 0.0, 1.0 );
+    //         // calculate diffuse
+    //         vec3 diffuse = u_PointColour[i] * max( dot( N, L ), 0.0 );
+    //         diffuse = clamp( diffuse, 0.0, 1.0 );
 
-            light += diffuse + specular;
+    //         // calculate specular
+    //         vec3 specular = vec3( 1.0 ) * pow( max( dot( R, E ), 0.0 ), 50.0 );
+    //         specular = clamp( specular, 0.0, 1.0 );
 
-            // // calculate the distance from the light
-            // float distance = length( u_PointPos[i] - v_vertex ) / u_PointDis[i];
-            // // calculate the direction from the light
-            // vec3 lightVector = normalize( u_PointPos[i] - v_vertex );
-            // // calculate the dot product between the normal and light vector
-            // float diffuse = max( dot( v_normal, lightVector ), 0.0 );
-            // // add attenuation
-            // float attenuation =
-            //     ( 1.0 / ( 1.0 + ( 0.25 * distance * distance ) ) );
-            // diffuse *= attenuation;
-            // // add colour
-            // vec3 diffuseVector = vec3( diffuse, diffuse, diffuse );
-            // diffuseVector *= u_PointColour[i];
+    //         light += diffuse + specular;
 
-            // // specular component
-            // // vec3 lightDirection =
-            // //     ( u_viewMatrix * vec4( u_PointPos[i], 1.0 ) ).xyz;
-            // // lightDirection = lightDirection + v_eyeDirection;
-            // // vec3 R = reflect( -lightDirection, normalize( lightDirection ) );
-            // // float specular = clamp( dot( v_eyeDirection, R ), 0.0, 1.0 );
-            // // specular = pow( specular, 5 ) / ( distance * distance );
-            // // vec3 specularVector = vec3( specular, specular, specular );
+    //         // // calculate the distance from the light
+    //         // float distance = length( u_PointPos[i] - v_vertex ) / u_PointDis[i];
+    //         // // calculate the direction from the light
+    //         // vec3 lightVector = normalize( u_PointPos[i] - v_vertex );
+    //         // // calculate the dot product between the normal and light vector
+    //         // float diffuse = max( dot( v_normal, lightVector ), 0.0 );
+    //         // // add attenuation
+    //         // float attenuation =
+    //         //     ( 1.0 / ( 1.0 + ( 0.25 * distance * distance ) ) );
+    //         // diffuse *= attenuation;
+    //         // // add colour
+    //         // vec3 diffuseVector = vec3( diffuse, diffuse, diffuse );
+    //         // diffuseVector *= u_PointColour[i];
 
-            // // vec3 R = reflect( -lightVector, v_normalCameraSpace );
-            // // vec3 viewDirection
+    //         // // specular component
+    //         // // vec3 lightDirection =
+    //         // //     ( u_viewMatrix * vec4( u_PointPos[i], 1.0 ) ).xyz;
+    //         // // lightDirection = lightDirection + v_eyeDirection;
+    //         // // vec3 R = reflect( -lightDirection, normalize( lightDirection ) );
+    //         // // float specular = clamp( dot( v_eyeDirection, R ), 0.0, 1.0 );
+    //         // // specular = pow( specular, 5 ) / ( distance * distance );
+    //         // // vec3 specularVector = vec3( specular, specular, specular );
 
-            // // add to total light
-            // // light += diffuseVector + specularVector;
-            // light += diffuseVector;
-        }
+    //         // // vec3 R = reflect( -lightVector, v_normalCameraSpace );
+    //         // // vec3 viewDirection
 
-        gl_FragColor = material * vec4(light, 1.0);
-    }
+    //         // // add to total light
+    //         // // light += diffuseVector + specularVector;
+    //         // light += diffuseVector;
+    //     }
+
+    //     gl_FragColor = material * vec4(light, 1.0);
+    // }
 }
--- res/gfx/shader/omicron/default_vertex.glsl
+++ res/gfx/shader/omicron/default_vertex.glsl
@@ -4,6 +4,8 @@
 //                                   VARIABLES
 //------------------------------------------------------------------------------
 
+//-----------------------------------UNIFORM------------------------------------
+
 // the model matrix
 uniform mat4 u_modelMatrix;
 // the view matrix
@@ -15,19 +17,30 @@ uniform mat4 u_normalMatrix;
 // the model, view, projection matrix
 uniform mat4 u_modelViewProjectionMatrix;
 
-// the vertex coords
-varying vec3 v_vertex;
+// the number of point lights
+uniform int u_PointCount;
+// the point light positions
+uniform vec3 u_PointPos[8];
+// the point light distances
+uniform float u_PointDis[8];
+// the point light colours
+uniform vec3 u_PointColour[8];
+
+// REMOVE ME
+// the colour
+uniform vec4 u_colour;
+
+//-----------------------------------VARYING------------------------------------
+
 // the texture coords
 varying vec2 v_texCoord;
-// the normals
-varying vec3 v_normal;
+// TESTING
+varying vec4 v_colour;
 
-// the vertex position in camera space
-varying vec3 v_vertexCameraSpace;
-// the direction of the eye
-varying vec3 v_eyeDirection;
-// the normal in camera space
-varying vec3 v_normalCameraSpace;
+// // the vertex coords
+// varying vec3 v_vertex;
+// // the normals
+// varying vec3 v_normal;
 
 //------------------------------------------------------------------------------
 //                                 MAIN FUNCTION
@@ -35,19 +48,46 @@ varying vec3 v_normalCameraSpace;
 
 void main()
 {
-    //set the vertex coord
-    v_vertex = vec3( u_modelMatrix * gl_Vertex );
-    //set the normal
-    v_normal = vec3( u_normalMatrix * vec4( gl_Normal, 0.0 ) );
     //set the texture coords
     v_texCoord = vec2( gl_MultiTexCoord0 );
+    // //set the vertex coord
+    // v_vertex = vec3( u_modelMatrix * gl_Vertex );
+    // //set the normal
+    // v_normal = vec3( u_normalMatrix * vec4( gl_Normal, 0.0 ) );
 
-    // calculate the vertex position in camera space
-    v_vertexCameraSpace = ( u_modelViewMatrix * gl_Vertex ).xyz;
-    // calculate the eye direction
-    v_eyeDirection = normalize( -v_vertexCameraSpace );
+    //---------------------------------LIGHTING---------------------------------
+    // TODO: these should come from material/light
+    float shininess = 128.0;
 
-    v_normalCameraSpace = (u_normalMatrix * vec4( gl_Normal, 0.0 ) ).xyz;
+    // transform the normal into eye space
+    vec3 normal = normalize( vec3( u_normalMatrix * vec4( gl_Normal, 0.0 ) ) );
+
+    // get the eye position
+    // TODO: should this just use model view?????
+    vec3 eyePos = -vec3( u_modelViewProjectionMatrix * gl_Vertex );
+
+    // transform the light position into eye space
+    vec3 lightPos = vec3( u_viewMatrix * vec4( u_PointPos[0], 0.0 ) );
+    // get the direction of the light
+    vec3 lightDir = normalize( lightPos );
+
+    // half vector
+    vec3 half = normalize( lightPos + eyePos );
+
+    // compute the cosine of the angle between the normal and light direction
+    float cosThetha = max( dot( normal, lightDir), 0.0 );
+
+    // specular
+    float specular = 0.0;
+    if ( cosThetha > 0.0 )
+    {
+        float cosAlpha = max( dot( normal, half ), 0.0 );
+        specular = pow( cosAlpha, shininess );
+    }
+
+    vec3 colour = cosThetha * u_PointColour[0];
+    colour += vec3( specular, specular, specular );
+    v_colour = u_colour * vec4( colour, 1.0 );
 
     //set the position
     gl_Position = u_modelViewProjectionMatrix * gl_Vertex;
